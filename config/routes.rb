9a9f724ac0d543cfb21e6e57d84a9cee9330df71
Allan Prikhodko <aprikhodko2012@gmail.com>
2015-09-04 19:29:55 +0000

    changed the routes.rb file removing previous commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'users/new'
-  match ':controller(/:Action(/:id))(.:format)'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
