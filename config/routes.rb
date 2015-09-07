bafa27fae280e8a08e60ccd6876999febf7bbc7b
Allan Prikhodko <aprikhodko2012@gmail.com>
2015-09-07 18:34:22 +0000

    Removed content from routes.rb in meantime, causing errors; migrated databse to MySQL; addded private pub functionality to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,3 @@
-UserAuth::Application.routes.draw do
-
-  match ':controller(/:action(/:id))(.:format)'
-  root :to => 'sessions#login'
-  match "signup", :to => "users#new"
-  match "login", :to => "sessions#login"
-  match "logout", :to => "sessions#logout"
-  match "home", :to => "sessions#home"
-  match "profile", :to => "sessions#profile"
-  match "setting", :to => "sessions#setting"
-end
-  
-  get 'users#new'
+Rails.application.routes.draw do
  
-   devise_for :users
-  
-    authenticated :user do
-      root 'users#index'
-    end
-  
-    unauthenticated :user do
-      devise_scope :user do
-        get "/" => "devise/sessions#new"
-      end
-    end
-  
-    resources :conversations do
-      resources :messages
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,33 +14,35 @@
 ActiveRecord::Schema.define(version: 20150904204440) do
 
   create_table "conversations", force: :cascade do |t|
-    t.integer  "sender_id"
-    t.integer  "recipient_id"
+    t.integer  "sender_id",    limit: 4
+    t.integer  "recipient_id", limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "conversations", ["recipient_id"], name: "index_conversations_on_recipient_id"
-  add_index "conversations", ["sender_id"], name: "index_conversations_on_sender_id"
+  add_index "conversations", ["recipient_id"], name: "index_conversations_on_recipient_id", using: :btree
+  add_index "conversations", ["sender_id"], name: "index_conversations_on_sender_id", using: :btree
 
   create_table "messages", force: :cascade do |t|
-    t.text     "body"
-    t.integer  "conversation_id"
-    t.integer  "user_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.text     "body",            limit: 65535
+    t.integer  "conversation_id", limit: 4
+    t.integer  "user_id",         limit: 4
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
   end
 
-  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id"
-  add_index "messages", ["user_id"], name: "index_messages_on_user_id"
+  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "email"
-    t.string   "encrypted_password"
-    t.string   "salt"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.string   "username",           limit: 255
+    t.string   "email",              limit: 255
+    t.string   "encrypted_password", limit: 255
+    t.string   "salt",               limit: 255
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
   end
 
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
 end
